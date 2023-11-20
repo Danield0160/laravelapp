22d5e6ed62e37772046f124abe68e804731975c9
Daniel windows <daniel.rosles@gmail.com>
2023-11-20 21:04:12 +0000

    v1 inicio hecho

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Bienvenido <?php echo session("usuario") ?></title>
+    <title>Bienvenido Inicio</title>
 </head>
 <body>
-    
+    <h1>Hola <?php echo session("usuario") ?></h1>
 </body>
 </html>
