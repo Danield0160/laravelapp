d61971a6ebaaaeeecd648a4bc22d2df2eeb0d4be
Daniel windows <daniel.rosles@gmail.com>
2023-11-23 21:47:47 +0000

    ajustes

--- resources/views/components/layouts/tabla.blade.php
+++ resources/views/components/layouts/tabla.blade.php
@@ -1,12 +1,20 @@
 <?php
-$datosLista = json_decode( str_replace("&quot;",'"',$datos),true);
-var_dump(array_keys($datosLista[0]));
+
+$datosLista = json_decode( html_entity_decode($datos),true);
 
 echo "<table>";
 
-
+echo "<tr>";
+foreach (array_keys($datosLista[0]) as $key) {
+        echo "<td>";
+        echo $key;
+        echo "</td>";
+}
+echo "</tr>";
 
 foreach ($datosLista as $row) {
+        echo "<tr>";
+
         echo '<td>';
         echo $row["id"];
         echo '</td>';
