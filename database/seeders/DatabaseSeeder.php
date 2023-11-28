420b7418a9842b5878f06aec916b6e6bd164eda1
Daniel Rosales <daniel.rosles@gmail.com>
2023-11-28 12:13:52 +0000

    migracion wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,6 @@
 <?php
-
 namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -12,11 +12,64 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table("usuarios")->insert([
+            "id"=>0,
+            "nombre"=>"Admin",
+            "clave"=>"Csas1234",
+            "admin"=>1
+        ]);
+        DB::table("usuarios")->insert([
+            "id"=>1,
+            "nombre"=>"Daniel",
+            "clave"=>"Csas1234",
+            "admin"=>1
+        ]);
+        DB::table("usuarios")->insert([
+            "id"=>2,
+            "nombre"=>"Pedro",
+            "clave"=>"Csas1234",
+            "admin"=>1
+        ]);
+        DB::table("usuarios")->insert([
+            "id"=>3,
+            "nombre"=>"Aitziber",
+            "clave"=>"Csas1234",
+            "admin"=>1
+        ]);
+        DB::table("usuarios")->insert([
+            "id"=>4,
+            "nombre"=>"Acaymo",
+            "clave"=>"Csas1234",
+            "admin"=>1
+        ]);
+        DB::table("usuarios")->insert([
+            "id"=>5,
+            "nombre"=>"Alberto",
+            "clave"=>"Csas1234",
+            "admin"=>1
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+        
+        DB::table("libros")->insert([
+            "id"=>0,
+            "nombre"=>"Cenicienta",
+            "unidades"=>5
+        ]);
+        DB::table("libros")->insert([
+            "id"=>1,
+            "nombre"=>"pocahontas",
+            "unidades"=>1
+        ]);
+        DB::table("libros")->insert([
+            "id"=>2,
+            "nombre"=>"El quijote",
+            "unidades"=>10
+        ]);
+        DB::table("libros")->insert([
+            "id"=>3,
+            "nombre"=>"Willy wonka",
+            "unidades"=>4
+        ]);
     }
 }
